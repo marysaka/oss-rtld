4117cf25bc2242b8e605a9aac67ab2504c89f8d7
Thog <me@thog.eu>
2019-04-01 12:27:59 +0200

    Remove duplicate UserExceptionHandler definition

--- source/exception_handler.cpp
+++ source/exception_handler.cpp
@@ -1,15 +1,6 @@
 #include "rtld.hpp"
 #include "sdk_init.hpp"
 
-namespace nn {
-namespace os {
-namespace detail {
-// FIXME: We set this weak to avoid the compiler to optimize the check we have.
-void WEAK UserExceptionHandler(void);
-}  // namespace detail
-}  // namespace os
-}  // namespace nn
-
 bool g_IsExceptionHandlerReady;
 
 extern "C" void __rtld_notify_exception_handler_ready(void) {
